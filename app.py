ff0e78b78d1062092052a278a82dd5faf48d3796
kannakamalesh1-cloud <kannakamalesh1@gmail.com>
2026-01-22 18:19:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-cat > app.py << 'EOF'
 from flask import Flask, render_template, request, jsonify
 import requests
 import os
@@ -79,4 +78,4 @@ def health_check():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
-EOF
+
